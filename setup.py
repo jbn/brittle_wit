001c53521db094ad09a942ac59963e038bba9117
John B Nelson <jbn@abreka.com>
2016-12-14 10:37:24 -0800

    Add missing manifest

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ from distutils.core import setup
 
 setup(
     name="brittle_wit",
-    version="0.0.1",
+    version="0.0.2",
+    author="John Bjorn Nelson",
+    author_email="jbn@pathdependent.com",
     packages=["brittle_wit",],
+    keywords=["twitter"],
     license="MIT",
+    description="An async Twitter library for Python.",
     long_description=open("README.md").read(),
+    url="https://github.com/jbn/BrittleWit",
     package_data={"brittle_wit": ["api_reference.json"]},
     include_package_data=True,
 )
