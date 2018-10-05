5efe463e45b9fd227cfe8c612f3e202c259ee4fc
John Bjorn Nelson <jbn@abreka.com>
2018-10-05 16:22:27 -0700

    Upgrade for 3.6

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.fixture
 def ignore_unawaited_request(recwarn):
     yield
-    m = "coroutine '_request' was never awaited"
+    m = "coroutine 'ClientSession._request' was never awaited"
     obs_messages = {str(w.message) for w in recwarn}
     assert obs_messages == set([m])
